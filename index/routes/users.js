b355bffe9f6b09ca6c7902df3d994c228bb279fd
Chris Cantu <pinion31@gmail.com>
2019-08-08 21:24:09 -0500

    Added pw validation on login

--- index/routes/users.js
+++ index/routes/users.js
@@ -10,7 +10,10 @@ router.post('/create-user', (req, res) => {
     if (err) res.status(500).json({ error: err.message ? err.message : err });
     bcrypt.hash(password1, salt, (err1, hash) => {
       if (err1) res.status(500).json({ error: err1.message ? err1.message : err1 });
-      User.create({ login: username, password: hash, addresses: [address] })
+      console.log(username, password1, address);
+      User.create({
+        login: username, password: hash, addresses: [address], etherAmount: 0,
+      })
         .then((user) => {
           res.status(200).json(user);
         });
@@ -22,9 +25,15 @@ router.post('/get-user', (req, res) => {
   const { username, password } = req.body;
   User.findOne({ where: { login: username } })
     .then((user) => {
-      if (user) return res.status(200).json(user);
-      return res.status(200).json({ error: 'Invalid username/password' });
-    });
+      if (!user) return res.status(200).json({ error: 'No User Found' });
+      const hashedPassword = user.password;
+      bcrypt.compare(password, hashedPassword, (err, result) => {
+        if (err) throw Error('Error validating user.');
+        console.log('res', result);
+        if (result) return res.status(200).json(user);
+        return res.status(200).json({ error: 'Invalid username/password' });
+      });
+    }).catch(({ message }) => res.status(200).json({ error: message }));
 });
 
 
