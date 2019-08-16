42abcf927f761bd4ca25b8bed073ceb19708cae1
Chris Cantu <pinion31@gmail.com>
2019-08-15 22:33:56 -0500

    Add MenuBar test

---
+++ __tests__/clientTests/MenuBar.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, cleanup } from 'react-testing-library';
+import { ReduxWrapper } from '../../src/components/ReduxWrapper';
+import MenuBar from '../../src/components/functional/MenuBar.jsx';
+
+afterEach(cleanup);
+
+const flushPromise = () => new Promise((resolve) => {
+  setTimeout(resolve, 0);
+});
+
+test('it loads and displays Bet Card', async () => {
+  const { getByText } = render(
+    <ReduxWrapper>
+      <MenuBar />
+    </ReduxWrapper>,
+  );
+
+  await flushPromise();
+  expect(getByText('Today\'s Events')).toBeDefined();
+  expect(getByText('Browse Events')).toBeDefined();
+  expect(getByText('Your Bets')).toBeDefined();
+  expect(getByText('Stats')).toBeDefined();
+  expect(getByText('Login')).toBeDefined();
+});
