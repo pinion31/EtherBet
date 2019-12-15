bd93f6eb2e3862ab6eaa2f4ca772ee5944ddc7fd
Chris Cantu <pinion31@gmail.com>
2019-12-15 14:04:17 -0600

    Add var for cron time

--- index/helpers/cron.js
+++ index/helpers/cron.js
@@ -3,8 +3,9 @@ import cron from 'node-cron';
 import { getEventsforFutureDays } from './rundown_api';
 import logger from '../logger';
 
+
 export const startCronJob = () => {
-  const task = cron.schedule('1 0 * * Mon', () =>  {
+  const task = cron.schedule(process.env.CRON_TIME, () =>  {
     getEventsforFutureDays(1);
     logger.info(`Starting cron job to pull sports events at ${new Date(Date.now())}`);
   }, {
