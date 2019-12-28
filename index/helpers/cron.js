9f4a4596e05e3ce23923f44f0c4c4bb42597262f
Chris Cantu <pinion31@gmail.com>
2019-12-28 12:14:05 -0600

    Add cron job to resolve bets

--- index/helpers/cron.js
+++ index/helpers/cron.js
@@ -1,16 +1,18 @@
 /* eslint-disable import/prefer-default-export */
 import cron from 'node-cron';
 import { getEventsforFutureDays } from './rundown_api';
+import { settleAllBets } from './betHelper';
 import logger from '../logger';
 
 
 export const startCronJob = () => {
-  const task = cron.schedule(process.env.CRON_TIME, () => {
+  const task = cron.schedule(process.env.CRON_TIME, async () => {
     const daysOffset = parseInt(process.env.DAYS_OFFSET, 10) || 0;
     const daysCount = parseInt(process.env.DAYS_COUNT, 10) || 1;
-    getEventsforFutureDays(daysCount, daysOffset);
     logger.info(`Starting cron job to pull sports events at ${new Date(Date.now())}`);
     logger.info(`Days Offset ${daysOffset} Days Count:${daysCount}`);
+    await getEventsforFutureDays(daysCount, daysOffset);
+    await settleAllBets();
   }, {
     scheduled: true,
     timezone: 'America/Chicago',
