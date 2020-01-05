e87fdb5e022e8f51b1490bcd07b8149c992c39e3
Chris Cantu <pinion31@gmail.com>
2020-01-04 23:22:50 -0600

    Added redux-persist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,9 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import Home from './Home.jsx';
 import Landing from './Landing.jsx';
 import Login from './Login.jsx';
@@ -13,11 +16,20 @@ import Bets from './Bets.jsx';
 import Signup from './Signup.jsx';
 import rootReducer from '../reducers/rootReducer';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleware = applyMiddleware(thunk, logger);
-const store = createStore(rootReducer, middleware);
+const store = createStore(persistedReducer, middleware);
+const persistor = persistStore(store);
 
 const App = () => (
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <Router>
         <Route exact path="/" component={Landing} />
         <Route exact path="/login" component={Login} />
@@ -27,6 +39,7 @@ const App = () => (
         <Route exact path="/your-bets" component={Bets} />
         <Route exact path="/stats" component={Stats} />
       </Router>
+    </PersistGate>
   </Provider>
 );
 
