2acb1cb1e6e6b557999db353b3664bf92eaaf8e6
Chris Cantu <pinion31@gmail.com>
2020-01-05 00:01:55 -0600

    Fix refresh error on pages other than landing

--- index/index.js
+++ index/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const session = require('express-session');
 const path = require('path');
+// const redis = require('redis');
+// const RedisStore = require('connect-redis')(session);
 
+// const client = redis.createClient();
 const app = express();
 const bodyParser = require('body-parser');
 const events = require('./routes/events');
@@ -10,15 +14,26 @@ const logger = require('./logger');
 const { startCronJob } = require('./helpers/cron');
 
 app.use(bodyParser.json());
+app.use(session({
+  secret: 'test',
+  // store: new redisStore({ host: 'localhost', port: 6379, client: client,ttl : 260}),
+  saveUninitialized: true,
+  resave: true,
+}));
 
 app.use(express.static(path.join(__dirname, '../dist')));
+// app.use('*', (req, res, next) => {
+//   console.log('res.session', req.session);
+//   console.log('cookie check');
+//   next();
+// });
 
 app.use('/events', events);
 app.use('/bets', bets);
 app.use('/users', users);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../index.html'));
+  res.sendFile(path.resolve(__dirname, '../dist/index.html'));
 });
 
 // if (process.env.NODE_ENV !== 'test') {
