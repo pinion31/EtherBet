133cfd364da846f66f77f1a03b28d002d37d85dd
Chris Cantu <pinion31@gmail.com>
2020-01-12 16:33:49 -0600

    Refresh sports cache with cron

--- index/helpers/cron.js
+++ index/helpers/cron.js
@@ -1,8 +1,10 @@
 /* eslint-disable import/prefer-default-export */
 import cron from 'node-cron';
-import { getEventsforFutureDays } from './rundown_api';
+import { getEventsforFutureDays, getSportsFromDB } from './rundown_api';
 import { settleAllBets } from './betHelper';
 import logger from '../logger';
+
+import { client } from './redisClient';
 
 
 export const startCronJob = () => {
@@ -11,6 +13,14 @@ export const startCronJob = () => {
     const daysCount = parseInt(process.env.DAYS_COUNT, 10) || 1;
     logger.info(`Starting cron job to pull sports events at ${new Date(Date.now())}`);
     logger.info(`Days Offset ${daysOffset} Days Count:${daysCount}`);
+    await getSportsFromDB()
+      .then((result) => {
+        if (!result) return logger.info('Error resetting cache for Sports');
+        logger.info(`Resetting Cache for Sports: ${result}`);
+        client.set('sports', JSON.stringify(result));
+      });
+
+
     await getEventsforFutureDays(daysCount, daysOffset);
     await settleAllBets();
   }, {
