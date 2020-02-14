a7ba4c7d9cc93451f385435d449189644e35d9a2
Chris Cantu <pinion31@gmail.com>
2020-02-13 21:23:26 -0600

    Add new jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   moduleNameMapper: {
-    transformIgnorePatterns: ['/node_modules/'],
+    transformIgnorePatterns: ['/node_modules/', '.css'],
+    '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules',
     // module must come first
     // '\\.module\\.css$': 'identity-obj-proxy',
     // '\\.css$': require.resolve('./test/style-mock.js'),
