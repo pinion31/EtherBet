6ec272ebd902e9910c19904ec48d00cc14b4f2f1
Chris Cantu <pinion31@gmail.com>
2020-05-24 12:13:49 -0500

    Temporarily disabled server tests

--- __tests__/testHelpers/ReduxWrapper.jsx
+++ __tests__/testHelpers/ReduxWrapper.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { BrowserRouter as Router } from 'react-router-dom';
-import rootReducer from '../../src/reducers/rootReducer';
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { BrowserRouter as Router } from "react-router-dom";
+import rootReducer from "../../src/reducers/rootReducer";
 
 const middleware = applyMiddleware(thunk);
 export let store = createStore(rootReducer, middleware);
 
-export const resetStore = () => store = createStore(rootReducer, middleware);
+export const resetStore = () => (store = createStore(rootReducer, middleware));
 
-export const ReduxWrapper = props => (
+export const ReduxWrapper = (props) => (
   <Provider store={props.store || store}>
-    <Router>
-      {props.children}
-    </Router>
+    <Router>{props.children}</Router>
   </Provider>
-
 );
